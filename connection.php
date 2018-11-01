33511ea8a6ab1df6b9e5135fb2b500acc061d014
AeroPwn <30424201+AeroPwn@users.noreply.github.com>
2018-10-31 18:15:00 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 define('DB_HOST', '');
 define('DB_NAME', '');
 define('DB_USER', '');
-define('DB_PASSWORD', '');
+define('DB_PASS', '');
 try{
 	$db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
 }
