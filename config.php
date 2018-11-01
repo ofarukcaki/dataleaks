da687ac14c767d2e42162fc405e20f79ee6867f6
AeroPwn <30424201+AeroPwn@users.noreply.github.com>
2018-10-31 18:18:05 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', '')
-define('DB_NAME', '')
-define('DB_USER', '')
-define('DB_PASS', '')
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 try{
 	$db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
 }
